611ff769129de0f9f4565df74046825b4fc0471c
Ricardo Paschoali <ricardo.paschoali@gmail.com>
2021-05-14 17:34:17 -0300

    first commit

---
+++ static/js/botoes_redirect.js
@@ -0,0 +1,8 @@
+    document.getElementById("botao-SDR").onclick = function () {
+    location.href = "http://191.209.73.99/sdr";  
+};
+
+document.getElementById("CRRP-Institucional").onclick = function () {
+    location.href = "http://casadoradioamador.org.br";  
+};
+
