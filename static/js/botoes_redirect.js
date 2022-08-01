0440c38c421cfd54b1ab6fa36fb8206e3e921428
Ricardo Paschoali <ricardo.paschoali@gmail.com>
2022-08-01 11:38:48 -0300

    updates

--- static/js/botoes_redirect.js
+++ static/js/botoes_redirect.js
@@ -1,8 +1,13 @@
-    document.getElementById("botao-SVX").onclick = function () {
-    location.href = "http://191.209.73.99/reflectorstatus";  
+document.getElementById("botao-SVX").onclick = function () {
+    location.href = "http://191.209.73.99/crrp-svx";
 };
 
 document.getElementById("CRRP-Institucional").onclick = function () {
-    location.href = "http://casadoradioamador.org.br";  
+    location.href = "http://casadoradioamador.org.br";
 };
+
+document.getElementById("botao-PY2KAK").onclick = function () {
+    location.href = "http://191.209.73.99/py2kak";
+};
+
 
