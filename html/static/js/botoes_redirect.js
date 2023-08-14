c92dbff20927336c3b33e406f73bfed1c2433de2
Ricardo Paschoali <ricardo.paschoali@gmail.com>
2023-08-14 00:46:23 -0300

    atualizaçao site inicial

--- html/static/js/botoes_redirect.js
+++ html/static/js/botoes_redirect.js
@@ -1,11 +1,23 @@
-document.getElementById("botao-SVX").onclick = function () {
-    location.href = "http://crrp.org.br/svxlink";
-};
-
 document.getElementById("CRRP-Institucional").onclick = function () {
     location.href = "http://casadoradioamador.org.br";
 };
 
 document.getElementById("CRRP-VPNLIST").onclick = function () {
     location.href = "http://crrp.org.br/vpnlist";
+};
+
+document.getElementById("botao-Reflector").onclick = function () {
+    location.href = "https://crrp.org.br/logs/reflector/";
+};
+
+document.getElementById("botao-PY2KAR").onclick = function () {
+    location.href = "https://crrp.org.br/logs/py2kar/";
+};
+
+document.getElementById("botao-PY2KCG").onclick = function () {
+    location.href = "https://crrp.org.br/logs/py2kcg/";
+};
+
+document.getElementById("botao-PY4AGC").onclick = function () {
+    location.href = "https://crrp.org.br/logs/py4agc/";
 };
